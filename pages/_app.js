22ace28d402c39ed17d0929af66f4e1dd8282d9d
Giwan Persaud <giwan.persaud@gmail.com>
2021-08-19 23:32:51 +0200

    warning fixes

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,12 @@ import Layout from "../components/Layout/Layout";
 import "../node_modules/prismjs/themes/prism-okaidia.css";
 import "../styles/globals.css";
 
-
 const App = function ({ Component, pageProps }) {
     return (
         <Layout pageTitle="Mytoori Blog" description="Mytoori Blog">
             <Component {...pageProps} />
         </Layout>
     );
-}
+};
 
 export default App;
