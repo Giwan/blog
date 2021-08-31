64b60d5dcd85e6d50e16a5031a50074524acd7f0
Giwan Persaud <giwan.persaud@gmail.com>
2021-08-31 21:10:20 +0200

    next version update

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,9 @@ import "../node_modules/prismjs/themes/prism-okaidia.css";
 import "../styles/globals.css";
 
 const App = ({ Component, pageProps }) => (
-        <Layout pageTitle="Mytoori Blog" description="Mytoori Blog">
-            <Component {...pageProps} />
-        </Layout>
-    );
+    <Layout pageTitle="Mytoori Blog" description="Mytoori Blog">
+        <Component {...pageProps} />
+    </Layout>
+);
 
 export default App;
