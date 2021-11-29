99d9aecc0dbab3fd4fa921223651f7284d6ac08e
Giwan Persaud <giwan.persaud@gmail.com>
2021-11-29 22:17:10 +0100

    header styling update

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -15,8 +15,10 @@ const Layout = ({ children, pageTitle, description }) => (
             <title>{pageTitle}</title>
         </Head>
         <Header />
-        <main className={styles.mainContent}>{children}</main>
-        <Footer />
+        <main className={styles.mainContent}>
+            {children}
+            <Footer />
+        </main>
     </div>
 );
 
