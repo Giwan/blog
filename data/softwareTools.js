3d6f900f9c81826a555921d20df97014c0763474
Giwan Persaud <giwan.persaud@gmail.com>
2022-02-27 18:33:03 +0100

    favicon and minor text updates

--- data/softwareTools.js
+++ data/softwareTools.js
@@ -28,6 +28,22 @@ const softwareTools = [
         category: categories.SOFTWARE,
         labels: [labels.productivity, labels.project],
     },
+    {
+        title: "webmonetization.org",
+        url: "https://webmonetization.org/",
+        description: "Monetize the web without dark patterns",
+        price: 0,
+        category: categories.SOFTWARE,
+        labels: [labels.productivity, labels.project],
+    },
+    {
+        title: "https://www.patreon.com",
+        url: "https://www.patreon.com",
+        description: "Monetization for creators",
+        price: 0,
+        category: categories.SOFTWARE,
+        labels: [labels.productivity, labels.project],
+    },
 ];
 
 export default softwareTools;
