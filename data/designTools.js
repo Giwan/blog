b7ad032f7b796162101dd3e8a375cacc84f424d9
Giwan Persaud <giwan.persaud@gmail.com>
2022-05-17 21:08:59 +0200

    line-height and tools

--- data/designTools.js
+++ data/designTools.js
@@ -61,7 +61,30 @@ const designTools = [
     {
         title: "Spline.design",
         url: "https://spline.design/", 
-        description: "Figma for 3D designs",
+        description: "Figma for 3D designs! Create 3D scenes, edit materials, and model 3D objects.",
+        price: 0,
+        category: categories.DESIGN,
+        labels: [labels.productivity, labels.prototype]
+    },
+    {
+        title: "reasonable.work",
+        url: "https://reasonable.work", 
+        description: `When working on a new project picking colours can be hard. 
+        This helps to find the right colour that is also accessible. 
+        Everybody benefits from accessible sites. 
+        I also like the simplicity of the site's design.
+        `,
+        price: 0,
+        category: categories.DESIGN,
+        labels: [labels.productivity, labels.prototype]
+    },
+    {
+        title: "octopus.do",
+        url: "https://octopus.do", 
+        description: `
+        A well designed app that helps visually build out your sitemap.
+        Useful to quickly visialise the different pages of your site. 
+        `,
         price: 0,
         category: categories.DESIGN,
         labels: [labels.productivity, labels.prototype]
